b84ae33671c1608505f0f99f00104aec9363b0ea
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-07-03 21:49:03 +0100

    Contact component completed

--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import Header from '../src/components/header/'
-import FilterControls from '../src/components/filterControls/'
+import Header from '../src/components/header/';
+import FilterControls from '../src/components/filterControls/';
+import Contact from '../src/components/contact/';
+
+const sample = {
+    name: {first:'Joe', last:'Bloggs'},
+    email: 'j.bloggs@example.com',
+    phone: '012-3456789',
+    picture: {thumbnail: './profile.png'}
+}
 
 storiesOf("Contact List App/Header", module).add("default", () => (
     <Header noContacts={10} />
@@ -11,4 +19,8 @@ storiesOf("Contact List App/Header", module).add("default", () => (
 
 storiesOf("Contact List App/Filter Controls", module).add("default", () => (
     <FilterControls />
+));
+
+storiesOf("Contact List App/Contact", module).add("default", () => (
+    <Contact contact={sample}/>
 ));
