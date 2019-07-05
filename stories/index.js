723298e55533a96cc0f0be5d30e1b1288af7d5b6
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-07-05 23:09:25 +0100

    PublicProfile component completed

--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,7 @@ import Contact from '../src/components/contact/';
 import ContactList from '../src/components/contactList/';
 import { action } from '@storybook/addon-actions';
 import { MemoryRouter, Route } from "react-router";
+import PublicProfile from '../src/components/contactPublic/publicProfile'
 
 const sample = {
     name: {first:'Joe', last:'Bloggs'},
@@ -72,3 +73,8 @@ storiesOf("Contact List App/Contact List", module)
         const samples = [sample, sample, sample, sample, sample]
         return <ContactList contacts={samples}/>
     });
+
+storiesOf("Contact List App/Contact Page/PublicProfile", module)
+    .add("default", () => (
+        <PublicProfile user={sample}/>
+    ));
