afd0e67e9a55b16857078dbc5d22e592db7327ee
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-07-05 22:01:01 +0100

    Filter contacts completed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import ContactList from "./components/contactList/";
 import FilterControls from "./components/filterControls/";
 import request from "superagent";
 import api from "./dataStore/stubAPI";
+import _ from "lodash";
 
 class App extends Component {
     state = { search: "", gender: "all" };
     deleteContact = (key) => {
         api.delete(key);
         this.setState({});
+    };
+    handleChange = (type, value) => {
+        type === "name"
+            ? this.setState({ search: value })
+            : this.setState({ gender: value });
     };
     componentDidMount() {
         request.get("https://randomuser.me/api/?results=50").end((error, res) => {
@@ -24,12 +30,24 @@ class App extends Component {
     }
     render() {
         let contacts = api.getAll();
+        let filteredContacts = contacts.filter(c => {
+            const name = `${c.name.first} ${c.name.last}`;
+            return name.toLowerCase().search(this.state.search.toLowerCase()) !== -1;
+        });
+        filteredContacts =
+            this.state.gender === "all"
+                ? filteredContacts
+                : filteredContacts.filter(c => c.gender === this.state.gender);
+        let sortedContacts = _.sortBy(filteredContacts, c => c.name.last);
+
         return (
             <div className="jumbotron">
-                <Header noContacts={contacts.length} />
-                <FilterControls />
-                <ContactList contacts={contacts}
-                             deleteHandler={this.deleteContact} />
+                <Header noContacts={sortedContacts.length} />
+                <FilterControls
+                    onUserInput={this.handleChange}
+                />
+                <ContactList contacts={sortedContacts}
+                             deleteHandler={this.deleteContact}  />
             </div>
         );
     }
